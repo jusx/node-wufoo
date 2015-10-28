534a26ec1658f936913bc935e05636079caf845a
jusx <justin@brightbytes.net>
2015-10-27 17:22:58 -0700

    fix webhook api call failures. Now using actual form post for the url param.

--- test/form.js
+++ test/form.js
@@ -4,37 +4,37 @@ var util = require("util");
 
 describe("Form", function() {
    var $form;
-   
+
    before(function(done) {
       helper.wufoo.getForms(function(err, forms){
          $form = forms[0];
          done(err);
       });
    })
-   
+
    describe("#getEntries", function() {
       it("Should return array of entries", function(done){
          $form.getEntries(function(err, entries){
             (entries.constructor == Array).should.be.true;
             helper.isEntry(entries[0]);
             done(err);
-         }); 
+         });
       })
-      
+
    });
-   
+
    describe("#getFields", function() {
       it("Should return array of fields", function(done){
          $form.getFields(function(err, fields){
             (fields.constructor == Array).should.be.true;
             helper.isField(fields[0]);
             done(err);
-         }); 
+         });
       })
-      
-      
+
+
    });
-   
+
    describe("#getEntriesCount", function() {
       it("Should return a number", function(done) {
          $form.getEntriesCount(function(err, count) {
@@ -43,7 +43,7 @@ describe("Form", function() {
          });
       });
    });
-   
+
    describe("#addWebhook and #deleteWebhook", function() {
       it("Should add a webhoook successfully", function(done) {
          $form.addWebhook("http://localhost:3000/bin", function(err, hashid) {
@@ -56,9 +56,9 @@ describe("Form", function() {
                done(err);
             });
          });
-         
+
       });
    })
-   
-   
+
+
 })
